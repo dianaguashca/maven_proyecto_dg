aebf73867e072531c6a5ea83e1630655e5c5ba95
Diana Guashca <dianaguashca18@gmail.com>
2021-09-06 20:09:50 -0500

    taller clase 13

---
+++ src/main/java1/ejercicios/variables/locales/globales/paciente.java
@@ -0,0 +1,20 @@
+package ejercicios.variables.locales.globales;
+
+public class paciente {
+      private String nombre;
+      private String apellido;
+      private int edad;
+      private int cedula;
+      
+      public void consulta() {
+    	  this.nombre="Alejandra";
+    	  this.apellido="factos";
+    	 this.edad=20;
+    	 this.cedula=1234;
+    	 
+    	 String horario="12 am";
+    	 horario="5 pm";
+    	 horario="3 pm";
+    	 
+      }
+}
