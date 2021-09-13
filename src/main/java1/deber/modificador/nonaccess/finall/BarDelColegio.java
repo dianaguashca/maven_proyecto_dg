1a61aedf3f76c2ef282aeb5aa7ad7273fa4fd45c
Diana Guashca <dianaguashca18@gmail.com>
2021-09-12 21:31:11 -0500

    primer finalll

--- src/main/java1/deber/modificador/nonaccess/finall/BarDelColegio.java
+++ src/main/java1/deber/modificador/nonaccess/finall/BarDelColegio.java
@@ -2,7 +2,7 @@ package deber.modificador.nonaccess.finall;
 
 //no se puede extender de una clase final
 //la clase (BarDelColegio) no compilara si la clase (ColegioPublico)
-//esta maraca como final
+//esta marcada como final
 public class BarDelColegio extends ColegioPublico{
 
 }
