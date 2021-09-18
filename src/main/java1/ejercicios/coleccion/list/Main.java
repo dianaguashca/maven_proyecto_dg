2a7def54e51d5a0b879397a73f45e8b45e0639ea
Diana Guashca <dianaguashca18@gmail.com>
2021-09-17 20:20:49 -0500

    del list

---
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -0,0 +1,24 @@
+package ejercicios.coleccion.list;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer numero1=1;
+		Integer numero2=4;
+		Integer numero3=8;
+		Integer numero4=20;
+		
+      List<Integer> listaNueva=new ArrayList<Integer>();
+      
+      listaNueva.add(numero1);//[0]
+      listaNueva.add(numero2);//[1]
+      listaNueva.add(numero3);//[2]
+      listaNueva.add(numero4);//[3]
+      
+	}
+
+}
